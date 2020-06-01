56037dbd5d61f639045d564b0a700386eecf6c06
Kayla Kingston <kkingston09@gmail.com>
2020-06-01 15:39:42 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,8 @@
 def reverse_each_word(string)
   array = string.split(" ")
   return_array = []
-  array.each << array.reverse
-    
-  
+  array.each do |string|
+    return_array << string.reverse 
+  end
+  return return_array
 end
